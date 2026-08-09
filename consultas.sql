a8c846cdcad47d79914ed3af3802155dd427bb51
Danilo Oliveira <dani2406oliver@gmail.com>
2026-08-09 19:16:23 -0300

    Update consultas.sql

--- consultas.sql
+++ consultas.sql
@@ -1,4 +1,13 @@
 -- para uma consulta normal para saber os funcionários cadastrados na tabela:
 SELECT * FROM funcionarios;
 
--
+--para consultar o registro por coluna:
+-- pela situação
+SELECT * FROM funcionarios WHERE situacao = 'ativo';
+
+-- pelo email:
+SELECT * FROM funcionarios WHERE email = 'emailusuarioA@gmail.com';
+
+-- pelo cargo:
+SELECT * FROM funcionarios WHERE id_cargo = 'id do cargo';
+
