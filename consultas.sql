dad6e0b442fe168a9a9e0fbf1162ba0645d47b4d
Danilo Oliveira <dani2406oliver@gmail.com>
2026-08-10 23:31:24 -0300

    consultas.sql

--- consultas.sql
+++ consultas.sql
@@ -11,3 +11,20 @@ SELECT * FROM funcionarios WHERE email = 'emailusuarioA@gmail.com';
 -- pelo cargo:
 SELECT * FROM funcionarios WHERE id_cargo = 'id do cargo';
 
+--consultar registros específicos apenas por um atributo de uma coluna inserido na tabela exemplo: nome de um funcionario:
+SELECT * FROM funcionarios WHERE nome_funcionario LIKE 'nomefuncionarioA%';
+
+-->OBS: usar o comando 'ILIKE' ignora erros de ortografia.
+
+--para consultas mais objetivas e de maneira mais ampla usaremos o comando JOIN :
+SELECT 
+departamento.id_departamento,
+departamento.nome_departamento,
+departamento.id_gerente,
+funcionarios.id_funcionario,
+funcionarios.nome_completo
+FROM departamentos
+JOIN funcionarios 
+ON departamento.id_gerente = funcionarios.id_funcionario;
+
+
