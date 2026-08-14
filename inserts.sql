c1449d5d8b9740458090d8710b2647ae171b0fb7
Danilo Oliveira <dani2406oliver@gmail.com>
2026-08-13 21:09:19 -0300

    inserts.sql

--- inserts.sql
+++ inserts.sql
@@ -1,2 +1,17 @@
---agora vamos inserir registros na tabela funicionario.
-INSERT INTO funcionarios(
+--agora vamos inserir registros na tabela funcionarios
+INSERT INTO funcionarios(nome_completo,funcao,situacao,telefone,email,cpf,data_nascimento) VALUES
+('Nome FuncionarioA','gerente departamental','Ativo','91-91111-1111','emailfuncionarioa@gmail.com','111.111.111-11','1990-01-01');
+INSERT INTO funcionarios(nome_completo,funcao,situacao,telefone,email,cpf,data_nascimento) VALUES
+('Nome FuncionarioB','supervisor geral','Ativo','91-92222-2222','emailfuncionariob@gmail.com','222.222.222-22','1998-02-05');
+INSERT INTO funcionarios(nome_completo,funcao,situacao,telefone,email,cpf,data_nascimento) VALUES
+('Nome FuncionarioC','secretario','Ativo','91-93333-3333','emailfuncionarioc@gmail.com','333.333.333-33','1999-12-03');
+INSERT INTO funcionarios(nome_completo,funcao,situacao,telefone,email,cpf,data_nascimento) VALUES
+('Nome FuncionarioD','logistica','Ativo','91-94444-4444','emailfuncionariod@gmail.com','444.444.444-11','1995-06-01');
+INSERT INTO funcionarios(nome_completo,funcao,situacao,telefone,email,cpf,data_nascimento) VALUES
+('Nome FuncionarioE','aux. administrativo','Ativo','91-95555-5555','emailfuncionarioe@gmail.com','555.555.555-91','2002-03-07');
+INSERT INTO funcionarios(nome_completo,funcao,situacao,telefone,email,cpf,data_nascimento) VALUES
+('Nome FuncionarioF','administrativo de vendas','Ativo','91-96666-5666','emailfuncionariof@gmail.com','666.565.666-66','1996-08-20');
+INSERT INTO funcionarios(nome_completo,funcao,situacao,telefone,email,cpf,data_nascimento) VALUES
+('Nome FuncionarioG','faxineiro','Ativo','91-97777-7777','emailfuncionariog@gmail.com','777.777.777-77','2003-10-15');
+INSERT INTO funcionarios(nome_completo,funcao,situacao,telefone,email,cpf,data_nascimento) VALUES
+('Nome FuncionarioH','faxineiro','Ativo','91-98787-8787','emailfuncionarioh@gmail.com','131.133.133-13','2004-09-05');
